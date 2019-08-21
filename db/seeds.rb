c9fd658f59b0f7d0b7e6876cd6d7362ae1ae03f8
Veronica Santos <verosbraga@gmail.com>
2019-08-21 15:20:47 +0100

    Models and seeds.rb populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+restaurants = Restaurant.create([{ name: 'Xing Li', address: 'Mihao street', phone_number: '23457890', category: 'chinese' }, { name: 'Mama Mia', address: 'Fontana street', phone_number: '65437654', category: 'italian' }, { name: 'Arigato', address: 'Jiraia street', phone_number: '12345678', category: 'japanese' }, { name: 'Entrecôte', address: 'Paris street', phone_number: '10293847', category: 'french' }, { name: 'Merci', address: 'Belgian street', phone_number: '65479876', category: 'belgian' }])
+
